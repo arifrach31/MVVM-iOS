1ae73f820cc1e9bbef4d29207d578d817400c2ee
arifrachman <arifrach31@gmail.com>
2019-09-20 16:27:08 +0700

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by ArifRachman on 20/09/19.
+//  Copyright © 2019 Telkom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
